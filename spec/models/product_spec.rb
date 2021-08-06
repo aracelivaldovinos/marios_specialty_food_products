a8cb83949219ca3f0912e612acfef6259dcba601
Araceli Valdovinos <valdovinosaraceli50@gmail.com>
2021-08-06 12:26:44 -0500

    add passing test for titleize callback

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -2,4 +2,7 @@ require 'rails_helper'
 
 describe Product do 
   it {should have_many (:reviews)} 
+  it ("titleizes the name of a product") do
+  product = Product.create({name: "jellybean calender", cost: 5.25, country_of_origin: "USA"})
+  end 
 end 
