d0b032d0620f79a4d977724f4c3e98718b54706f
Araceli Valdovinos <valdovinosaraceli50@gmail.com>
2021-08-06 14:58:25 -0500

    add seed data to reviews

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,7 +4,18 @@ require 'faker'
   Product.create!(
     name: Faker::Coffee.blend_name,
     cost: Faker::Number.decimal(l_digits: 2),
-    country_of_origin: Faker::Coffee.origin
+    country_of_origin: Faker::Coffee.origin,
+    id: Faker::Number.unique.between(from: 1, to: 50)
 )
 end 
+
+250.times do 
+  Review.create!(
+    author: Faker::Name.name, 
+    content_body: Faker::Lorem.sentence(word_count: 20),
+    rating: Faker::Number.within(range: 1..5),
+    product_id: Faker::Number.between(from: 1, to: 50)
+  )
+end 
+
 
