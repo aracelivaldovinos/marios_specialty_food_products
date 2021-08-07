e62725d7e787cf8650bc393a93b1ab8461e8fd57
Araceli Valdovinos <valdovinosaraceli50@gmail.com>
2021-08-06 19:21:42 -0500

    add scope for all product sold in latin america

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,11 @@
 class Product < ApplicationRecord
   has_many :reviews
   scope :three_most_recent, -> { order(created_at: :desc).limit(3)}
+  scope :panama_location, -> { where(country_of_origin: "Chiriqui, Panama")}
+  scope :nicaragua_location, -> { where(country_of_origin: "Madriz, Nicaragua")}
+  scope :el_salvador_location, -> { where(country_of_origin: "Tecapa-Chinameca, El Salvador")}
+  scope :brazil_location, -> { where(country_of_origin: "Sul Minas, Brazil")}
+
 
   before_save(:titleize_product)
   private 
